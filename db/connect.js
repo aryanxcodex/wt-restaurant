c4b488150e10212b92764e2b04fca2b293c15f4f
aryanxcodex <s424.aryansingh@gmail.com>
2024-10-23 15:48:56 +0530

    Added backend routes

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,18 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbName = process.env.DB_NAME;
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'your_username',    
-    password: 'your_password', 
-    database: 'your_database'  
+    host: dbHost,
+    user: dbUser,    
+    password: dbPass, 
+    database: dbName,  
 });
 
 connection.connect((err) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,40 @@
 import express from 'express';
-
+import connection from '../db/connect.js'; 
+import bcrypt from 'bcrypt'; 
 const router = express.Router();
 
+router.post('/register', async (req, res) => {
+    const { username, password, email } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10); 
+        const [result] = connection.query('INSERT INTO Users (username, password, email) VALUES (?, ?, ?)', [username, hashedPassword, email]);
+        res.status(201).json({ message: 'User registered successfully'});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
-// // import { getAllUsers, getUserById, createUser, updateUser, deleteUser } from './controllers/userController.js';
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const [rows] = await pool.query('SELECT * FROM Users WHERE username = ?', [username]);
+        
+        if (rows.length === 0) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
 
-// router.get('/', getAllUsers);
+        const user = rows[0];
+        const isPasswordValid = await bcrypt.compare(password, user.password);
 
-// router.get('/:id', getUserById);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
 
-// router.post('/', createUser);
+        res.status(200).json({ message: 'Login successful', userId: user.user_id, username: user.username });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
-// router.put('/:id', updateUser);
-
-// router.delete('/:id', deleteUser);
 
 export default router;
